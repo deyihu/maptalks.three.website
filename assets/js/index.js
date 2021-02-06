ee5a4b653eb75e73ab7ca870472638332ddc187b
deyihu <mehudeyi@163.com>
2021-02-06 22:35:38 +0800

    update

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,3 +1,3 @@
 $.getJSON('https://api.github.com/repos/maptalks/maptalks.three', function (data) {
-    $('#star-content').html('stars :'+data.stargazers_count)
+    $('#star-content').html('<i class="fa fa-star"></i>stars :'+data.stargazers_count)
 })
